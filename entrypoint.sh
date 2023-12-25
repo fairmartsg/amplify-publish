a997d122c7bf58e1a140f059e70649fe36ebc272
t0e <thetmgmgtoe7@gmail.com>
2023-12-26 01:26:22 +0400

    apply amplify changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -55,7 +55,7 @@ case $5 in
     ;;
 
   publish)
-    amplify publish $9 --yes && git status && git add * && git commit -m 'apply amplify changes' && git push
+    amplify publish $9 --yes && git status && git add --all && git commit -m 'apply amplify changes' && git push
     ;;
 
   status)
